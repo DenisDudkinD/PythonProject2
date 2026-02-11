9ef66aec4831b1fc4688bbe4d7eb813f3e268265
Denis Dudkin <denis.dudkin.d@gmail.com>
2026-02-11 11:52:02 -0500

    Updated actor repo

--- src/repositories/actor_repository.py
+++ src/repositories/actor_repository.py
@@ -10,11 +10,17 @@ class SQLActorRepository(ActorRepositoryProtocol):
     def add_actor(self, actor:Actor):
         self.session.add(actor)
         self.session.commit()
+
     def get_all_actors(self) -> list[Actor]:
         return self.session.query(Actor).all()
+    
     def get_actor_by_name(self,query:str):
-        self.session.query(Actor).filter(Actor.full_name == query).all()
-    def remove_actor_by_id(self,actor_id:str):
-        ...
-    def update_actor(self,actor_id:str,actor:Actor):
-        ...
+        return self.session.query(Actor).filter(Actor.full_name == query).all()
+
+    def remove_actor(self,actor:Actor):
+        self.session.delete(actor)
+        self.session.commit()
+
+    def update_actor(self,actor:Actor):
+        self.session.merge(actor)
+        self.session.commit()

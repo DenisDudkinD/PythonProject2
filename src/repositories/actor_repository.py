2e60d7b1e2af9bbe738121790f867f91ac5693ac
Denis Dudkin <denis.dudkin.d@gmail.com>
2026-02-11 11:42:31 -0500

    Fixed file extension and added some actor repo methods

--- src/repositories/actor_repository.py
+++ src/repositories/actor_repository.py
@@ -2,6 +2,19 @@ from sqlalchemy.orm import Session
 from src.domain.actor import Actor
 from src.repositories.actor_repository_protocol import ActorRepositoryProtocol
 
-class SQLBookRepository(ActorRepositoryProtocol):
+class SQLActorRepository(ActorRepositoryProtocol):
     def __init__(self, session: Session):
         self.session = session
+
+
+    def add_actor(self, actor:Actor):
+        self.session.add(actor)
+        self.session.commit()
+    def get_all_actors(self) -> list[Actor]:
+        return self.session.query(Actor).all()
+    def get_actor_by_name(self,query:str):
+        self.session.query(Actor).filter(Actor.full_name == query).all()
+    def remove_actor_by_id(self,actor_id:str):
+        ...
+    def update_actor(self,actor_id:str,actor:Actor):
+        ...

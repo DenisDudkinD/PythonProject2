573a5281c3e6a0628fee25ff8355c0d10be916ef
Liam O'Neill <liamoneill496@gmail.com>
2026-02-11 13:39:46 -0500

    add studio service

--- src/repositories/studio_repository.py
+++ src/repositories/studio_repository.py
@@ -6,7 +6,7 @@ class StudioRepository(StudioRepositoryProtocol):
     def __init__(self, session: Session):
         self.session = session
 
-    def add_studio(self, studio:Studio):
+    def add_studio(self, studio:Studio) -> str:
         self.session.add(studio)
         self.session.commit()
         return str(studio.studio_id)
@@ -15,14 +15,13 @@ class StudioRepository(StudioRepositoryProtocol):
     def get_all_studios(self) -> list[Studio]:
         return self.session.query(Studio).all()
 
-    def get_studio_by_name(self,query:str):
+    def get_studio_by_name(self,query:str) -> list[Studio]:
         return self.session.query(Studio).filter(Studio.name == query).all()
         
     def remove_studio_by_id(self,studio_id:str):
-        studio = self.session.query(Studio).filter(Studio.studio_id == studio_id).all()[0]
+        studio = self.session.get(Studio, studio_id)
         self.session.delete(studio)
         self.session.commit()
-        return str(studio.studio_id)
 
     def update_studio(self,studio_id:str,studio:Studio):
         self.remove_studio_by_id(studio_id)

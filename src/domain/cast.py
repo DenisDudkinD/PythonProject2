ea438202b5d2f593b93070bdb1db18dbb1fe3385
Denis Dudkin <denis.dudkin.d@gmail.com>
2026-02-18 15:30:55 -0500

    Added relationship to cast

--- src/domain/cast.py
+++ src/domain/cast.py
@@ -1,6 +1,7 @@
 from sqlalchemy import Column, String, ForeignKey, Integer
 from sqlalchemy.dialects.postgresql import UUID
 from src.base import Base
+from sqlalchemy.orm import relationship
 
 class Cast(Base):
     __tablename__ = "movie_cast"
@@ -12,3 +13,5 @@ class Cast(Base):
     character_name = Column(String, nullable=False)
     billing_order = Column(Integer,nullable=False)
 
+    actor = relationship("Actor", backref ="castings")
+    movie = relationship("Movie", backref ="castings")
